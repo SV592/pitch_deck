4be8614e0e8d8fb4ee33e10775f397c72aae1e91
Shaquille Pearson <shaquillepearson9@gmail.com>
2025-07-30 12:20:37 -0400

    include webpack config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
+import path from 'path';
+
 const nextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    config.resolve.modules.push(path.resolve('./node_modules'));
+    return config;
+  },
 };
 
 export default nextConfig;
